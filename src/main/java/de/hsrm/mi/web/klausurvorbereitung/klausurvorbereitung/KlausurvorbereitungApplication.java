42023785b47aca552a2d0593fd783404589dc3d7
David Thomann <david.thomann@student.hs-rm.de>
2025-02-02 00:27:23 +0100

    Initial commit

---
+++ src/main/java/de/hsrm/mi/web/klausurvorbereitung/klausurvorbereitung/KlausurvorbereitungApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.web.klausurvorbereitung.klausurvorbereitung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlausurvorbereitungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlausurvorbereitungApplication.class, args);
+	}
+
+}
