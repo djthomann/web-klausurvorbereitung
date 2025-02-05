913381ffd5c1f4a4ba6fef4e5cd0ed981583590c
David Thomann <david.thomann@student.hs-rm.de>
2025-02-06 00:14:16 +0100

    Added more HTMX usage

--- src/main/java/de/hsrm/mi/web/klausurvorbereitung/klausurvorbereitung/controller/FragmentController.java
+++ src/main/java/de/hsrm/mi/web/klausurvorbereitung/klausurvorbereitung/controller/FragmentController.java
@@ -3,13 +3,20 @@ package de.hsrm.mi.web.klausurvorbereitung.klausurvorbereitung.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-@RequestMapping("/fragments")
+@RequestMapping("/hx/fragments")
 public class FragmentController {
+   
+    @GetMapping("/schnipsel/{name}/{nummer}")
+    public String getMethodName(@PathVariable("name") String name, @PathVariable("nummer") int nummer) {
+        return "/frag/schnipsel :: schnipsel";
+    }
     
+
     @GetMapping("/header")
     public String getHeader() {
         return "/frag/header :: header";
